d199791c869bc63f7d082c92e34c8488c9223cd8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-02 16:59:44 -0400

    Code for "format.cpp"

--- CppND-System-Monitor-Project-Updated/src/format.cpp
+++ CppND-System-Monitor-Project-Updated/src/format.cpp
@@ -1,11 +1,29 @@
 #include <string>
+#include <vector>
 
 #include "format.h"
 
 using std::string;
 
+using namespace std;
+
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]])    // calculates Uptime into { Hr:Min:Sec }
+{
+    vector<int> temp_times{};    // holds sec AFTER calculation     { hr, min, sec }
+    vector<int> denominators{ 3600, 60, 1 };    // { hr, min, sec }
+
+    long sec = seconds;     // copy of original system uptime (WILL BE CHANGED TO FIND REAL UPTIME)
+
+    for (int i = 0; i < 3; i++)
+    {
+        temp_times[i] = int(floor(sec/temp_times[i]));   // floor() Rounds DOWN to int
+
+        sec -= temp_times[i] * denominators[i];     // subtracts new sec result from original sec
+    }
+    
+    return to_string(temp_times[0]) + ":" + to_string(temp_times[1]) + ":" + to_string(temp_times[2]);      // used '+' because '<<' gives error & only used for 'cout'      converts 'int' to 'string'      { hr, min, sec }
+}
