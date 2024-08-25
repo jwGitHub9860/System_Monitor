cde98050bd3cc9b3b12cd475c38f85dc88e3f364
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-25 13:43:22 -0400

    Changed & Added comments to "linux_parser.h"

--- CppND-System-Monitor-Project-Updated/include/linux_parser.h
+++ CppND-System-Monitor-Project-Updated/include/linux_parser.h
@@ -8,15 +8,15 @@
 namespace LinuxParser {
 // Paths
 const std::string kProcDirectory{"/proc/"};   // lots of system data location
-const std::string kCmdlineFilename{"/cmdline"};
+const std::string kCmdlineFilename{"/cmdline"}; // possible Command()
 const std::string kCpuinfoFilename{"/cpuinfo"}; // possible CpuUtilization()
 const std::string kStatusFilename{"/status"};
-const std::string kStatFilename{"/stat"};   // possible Jiffies(), ActiveJiffies(), AcitiveJiffies(int pid), IdleJiffies()
+const std::string kStatFilename{"/stat"};   // 1 possible User()                                                                                                                                                   Jiffies(), ActiveJiffies(), AcitiveJiffies(int pid), IdleJiffies()
 const std::string kUptimeFilename{"/uptime"}; // possible UpTime()
-const std::string kMeminfoFilename{"/meminfo"}; // possible MemoryUtilization()
-const std::string kVersionFilename{"/version"};
+const std::string kMeminfoFilename{"/meminfo"}; // possible Ram()                                                                                                                                                  MemoryUtilization()
+const std::string kVersionFilename{"/version"}; // possible Pid()
 const std::string kOSPath{"/etc/os-release"};   // stores operating system name
-const std::string kPasswordPath{"/etc/passwd"};
+const std::string kPasswordPath{"/etc/passwd"}; // 2 possible User()                                                                                                                                               User(int pid)
 
 // System
 float MemoryUtilization();
