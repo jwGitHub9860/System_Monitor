9ef6526143b47688925266a29e18bfda0d3a2346
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-26 20:02:00 -0400

    Corrections for User()

--- CppND-System-Monitor-Project-Updated/src/process.cpp
+++ CppND-System-Monitor-Project-Updated/src/process.cpp
@@ -31,19 +31,7 @@ string Process::Command() { return string(); }
 string Process::Ram() { return string(); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User()
-{
-    string user, line;
-    ifstream stream(LinuxParser::kPasswordPath);
-    if (stream.is_open())
-    {
-        getline(stream, line);
-        istringstream linestream(line);
-        linestream >> user;
-        return user;
-    }
-    return string();
-}
+string Process::User() { return user_name; }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
