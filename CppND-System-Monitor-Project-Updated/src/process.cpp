4ce538f083fad0b453e0fac4f7e6ae275fb9897a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-27 18:08:56 -0400

    Correction for Process::Command()

--- CppND-System-Monitor-Project-Updated/src/process.cpp
+++ CppND-System-Monitor-Project-Updated/src/process.cpp
@@ -25,7 +25,7 @@ float Process::CpuUtilization()
 }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return command; }
+string Process::Command() { return LinuxParser::Command(Pid); }
 
 // TODO: Return this process's memory utilization
 string Process::Ram() { return mem_usage; }
