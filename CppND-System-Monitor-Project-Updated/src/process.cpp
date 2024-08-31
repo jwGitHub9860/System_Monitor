193b93fee2533aa5699b9757e364584808fb04d3
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-31 19:38:11 -0400

    Code for Process::operator<(Process const& a)

--- CppND-System-Monitor-Project-Updated/src/process.cpp
+++ CppND-System-Monitor-Project-Updated/src/process.cpp
@@ -52,4 +52,7 @@ long int Process::UpTime() { return LinuxParser::UpTime() - LinuxParser::UpTime(
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const
+{
+    return (x < a.x) || ((x == a.x) && (y < a.y));
+}
