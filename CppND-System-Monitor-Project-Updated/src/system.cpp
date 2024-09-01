31d2a4801ff5a74d038d4e7ee32184dae89a6826
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-08-31 20:39:10 -0400

    Added comment to System::Cpu()

--- CppND-System-Monitor-Project-Updated/src/system.cpp
+++ CppND-System-Monitor-Project-Updated/src/system.cpp
@@ -17,7 +17,7 @@ using std::vector;
 using namespace std;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+Processor& System::Cpu() { return cpu_; }   // same as linux_parser.cpp?
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
