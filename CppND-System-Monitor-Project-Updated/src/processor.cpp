4d75787b69711225269c57ff2dbc8207078e2f58
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-18 20:29:43 -0400

    Corrections for "processor.cpp"

--- CppND-System-Monitor-Project-Updated/src/processor.cpp
+++ CppND-System-Monitor-Project-Updated/src/processor.cpp
@@ -3,8 +3,18 @@
 
 #include "processor.h"
 #include "linux_parser.h"
+#include "process.h"
 
 using namespace std;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return LinuxParser::CpuUtilization(); }
+float Processor::Utilization()
+{
+    long total_jiffies = LinuxParser::Jiffies();
+    long active_jiffies = LinuxParser::ActiveJiffies();
+    
+    float total_time = total_jiffies;   // change long to float
+    float active_time = active_jiffies;   // change long to float
+    
+    return (active_time / total_time) * total_time;     // CPU usage percent = (Active_CPU_time / Total_CPU_time) * 100         total_time = 100%
+}
