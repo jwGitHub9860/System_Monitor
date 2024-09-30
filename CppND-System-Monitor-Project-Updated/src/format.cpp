258e7b4e51cc5f5534afea402e7d6f6473413001
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-09-30 15:01:26 -0400

    Corrections for "format.cpp" file

--- CppND-System-Monitor-Project-Updated/src/format.cpp
+++ CppND-System-Monitor-Project-Updated/src/format.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 #include "format.h"
 
@@ -15,26 +17,13 @@ using namespace std;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds)    // calculates Uptime into { Hr:Min:Sec }
 {
-    long hr = seconds / 3600;
-    seconds = seconds % 3600;
-    long min = seconds / 60;
-    long sec = seconds % 60;
-    return to_string(hr) + ":" + to_string(min) + ":" + to_string(sec);
-    /*vector<int> temp_times{};    // holds sec AFTER calculation     { hr, min, sec }
-    vector<int> denominators{ 3600, 60, 1 };    // { hr, min, sec }
-
-    long sec = seconds;     // copy of original system uptime (WILL BE CHANGED TO FIND REAL UPTIME)
-
-    while (sec > 0)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            temp_times[i] = int(floor(sec/temp_times[i]));   // floor() Rounds DOWN to int
-
-            sec -= temp_times[i] * denominators[i];     // subtracts new sec result from original sec
-        }
-    }
-    
-    return to_string(temp_times[0]) + ":" + to_string(temp_times[1]) + ":" + to_string(temp_times[2]);      // outputs HH:MM:SS      used '+' because '<<' gives error & only used for 'cout'      converts 'int' to 'string'
-    */
+    int hours = seconds / 3600;
+    seconds %= 3600;
+    int minutes = seconds / 60;
+    seconds %= 60;
+    std::ostringstream stream;
+    stream << std::setw(2) << std::setfill('0') << hours << ":"
+            << std::setw(2) << std::setfill('0') << minutes << ":"
+            << std::setw(2) << std::setfill('0') << seconds;
+    return stream.str();
 }
